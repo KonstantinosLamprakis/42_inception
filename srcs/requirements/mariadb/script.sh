43b43637f80640678331f5a95721c13f73cfb796
KonstantinosLamprakis <28678729+KonstantinosLamprakis@users.noreply.github.com>
2024-08-07 16:25:09 +0200

    feat: fetch db info from env vars instead of hardcoding

---
+++ srcs/requirements/mariadb/script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+service mariadb start 
+sleep 5 
+
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASS}';"
+mariadb -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO \`${DB_USER}\`@'%';"
+mariadb -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root shutdown
+
+mysqld
--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sleep 10
 cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
