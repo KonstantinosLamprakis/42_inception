d60e13418b3b6e9da69c03e4743594645e6231d0
KonstantinosLamprakis <28678729+KonstantinosLamprakis@users.noreply.github.com>
2024-08-08 17:45:17 +0200

    fix: replace latest tag with another, replace health check with script

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
+sleep 10
+#---------------------------------------------------ping mariadb---------------------------------------------------#
+# check if mariadb container is up and running
+ping_mariadb_container() {
+    nc -zv mariadb 3306 > /dev/null # ping the mariadb container
+    return $? # return the exit status of the ping command
+}
+start_time=$(date +%s) # get the current time in seconds
+end_time=$((start_time + 20)) # set the end time to 20 seconds after the start time
+while [ $(date +%s) -lt $end_time ]; do # loop until the current time is greater than the end time
+    ping_mariadb_container # Ping the MariaDB container
+    if [ $? -eq 0 ]; then # Check if the ping was successful
+        echo "[========MARIADB IS UP AND RUNNING========]"
+        break # Exit the loop if MariaDB is up
+    else
+        echo "[========WAITING FOR MARIADB TO START...========]"
+        sleep 1 # Wait for 1 second before trying again
+    fi
+done
+
+if [ $(date +%s) -ge $end_time ]; then # check if the current time is greater than or equal to the end time
+    echo "[========MARIADB IS NOT RESPONDING========]"
+fi
+
 cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
